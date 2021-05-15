f0118327eab4fae9a0efb70ac2c88f3b339cdb2a
Owen Cook <66239220+OwenTheProgrammer@users.noreply.github.com>
2021-05-15 19:04:54 -0400

    Update with inline string formatting

--- AudioMod/Assertions.cs
+++ AudioMod/Assertions.cs
@@ -7,21 +7,19 @@ namespace AudioMod
         //All Console Logs are here
         public void AudioManager_LRByteError(byte[] Left, byte[] Right)
         {
-            Console.WriteLine("AudioManager: Left and Right Channel byte[] length doesnt match \n -> LeftChannel: {0} | RightChannel: {1}\n", Left.Length, Right.Length);
+            Console.WriteLine($"AudioManager: Left and Right Channel byte[] length doesnt match \n -> LeftChannel: {Left.Length} | RightChannel: {Right.Length}\n");
         }
         public void AudioManager_ByteSampleError(int samples, byte[] Left, byte[] Right)
         {
-            Console.WriteLine("AudioManager: Channel Byte array doesnt match sample count \n -> Samples: {0} | LeftChannel: {1} | RightChannel: {2}\n", samples, Left.Length, Right.Length);
+            Console.WriteLine($"AudioManager: Channel Byte array doesnt match sample count \n -> Samples: {samples} | LeftChannel: {Left.Length} | RightChannel: {Right.Length}\n");
         }
         public void AudioManager_FileCreationSuccess(String path)
         {
-            Console.WriteLine("AudioManager: Successfully created file: {0}", path);
+            Console.WriteLine($"AudioManager: Successfully created file: {path}");
         }
         public void AudioManager_LoadSplitError(int Samples, byte[] AudioByteStream, int sampleRate, int bitsPerSample, int Channels, int HeaderLength)
         {
-            Console.WriteLine("AudioManager: File header is too long, clip isnt long enough for current settings or SampleRate isnt correct \n ->" +
-                "Samples: {0} | RawStream: {1} | Headerless: {2} | SampleRate: {3} at {4} b/s | Audio Channels: {5} | Header Length: {6} | Aprox Header: {7}\n",
-                Samples, AudioByteStream.Length, (AudioByteStream.Length - HeaderLength), sampleRate, bitsPerSample, Channels, HeaderLength, (AudioByteStream.Length - Samples));
+            Console.WriteLine($"AudioManager: File header is too long, clip isnt long enough for current settings or SampleRate isnt correct \n -> Samples: {Samples} | RawStream: {AudioByteStream.Length} | Headerless: {(AudioByteStream.Length - HeaderLength)} | SampleRate: {sampleRate} at {bitsPerSample} b/s | Audio Channels: {Channels} | Header Length: {HeaderLength} | Aprox Header: {(AudioByteStream.Length - Samples)}\n");
         }
         public void AudioManager_AudioSampleNull()
         {
@@ -29,7 +27,7 @@ namespace AudioMod
         }
         public void AudioManager_FileNotFound(String f)
         {
-            Console.WriteLine("AudioManager: File ref ({0}) Not found.\n", f);
+            Console.WriteLine($"AudioManager: File ref ({f}) Not found.\n");
         }
         public void Main_CoreExecutionHalt()
         {
@@ -37,11 +35,11 @@ namespace AudioMod
         }
         public void General_FileCount(String[] Files, String Path)
         {
-            Console.WriteLine("{0} Files found in {1}\n", Files.Length, Path);
+            Console.WriteLine($"{Files.Length} Files found in {Path}\n");
         }
         public void AudioManager_ConvertedSignature(bool Direction)
         {
-            Console.WriteLine("AudioManager: Converting {0}\n", Direction ? "Signed -> Unsigned" : "Unsigned -> Signed");
+            Console.WriteLine($"AudioManager: Converting {(Direction ? "Signed -> Unsigned" : "Unsigned -> Signed")}\n");
         }
     }
 }
